ff5ceda95b54639639f2245d0b2ddf5b0147de37
nunomiguel22 <nunomiguel22@LENOVO-LAPTOP.localdomain>
2021-11-30 15:54:44 +0000

    new datasets

---
+++ vivino_aggregate_reviews.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+INFO_FILE = 'datasets/vivino-info.csv'
+REVIEW_FILE = 'datasets/vivino_reviews_2.csv'
+
+df_info = pd.read_csv(INFO_FILE)
+df_reviews = pd.read_csv(REVIEW_FILE)
+
+#df_reviews = df_reviews.drop(col3umns=['rating', 'user'], axis=1)
+
+df_reviews = df_reviews.groupby(['id'])['note'].apply(','.join).reset_index()
+
+df_final = pd.merge(df_info, df_reviews, how='left')
+
+#print(df_final)
+
+df_final.to_csv('datasets/vivino_reviews_final.csv', index=True)
