db265262d5a3d74ca4deeb08d85cadb7433c3a37
nunomiguel22 <nunomiguel22@gmail.com>
2022-01-05 19:40:02 +0000

    added missing imports

--- react-ts-frontend/src/components/pageLayout.tsx
+++ react-ts-frontend/src/components/pageLayout.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Footer from '../components/footer';
 import styles from '../styles/page.module.css';
 
--- react-ts-frontend/src/components/wineCard.tsx
+++ react-ts-frontend/src/components/wineCard.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Wine } from '../interfaces';
 import styles from '../styles/winecard.module.css';
 import { Link } from "react-router-dom";
